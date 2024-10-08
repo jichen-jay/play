b6adad52ac0d7094cc70e0adb568f43030f1adbb
jichen-jay <jaykchen@icloud.com>
2024-10-08 17:31:40 -0400

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,14 +1,6 @@
 time node dist/bundle.js "https://www.wsj.com/politics/elections/robert-f-kennedy-jr-drops-out-of-presidential-race-endorses-trump-f043e9b9?mod=hp_lead_pos1" 
-
 npx webpack --config webpack.config.js  --stats-error-details
-
-https://www.npmjs.com/package/cssnano-preset-advanced
-
-https://jsr.io/@std/http/doc
-
-https://github.com/jichen-jay?tab=repositories
-
-
 Xvfb :99 -ac &
 export DISPLAY=:99
 
+need a better way to record command history
